bf8cc81a59919032724ae878d9ab3f808f5e9395
JEEPERS CREEPER <lovertoxic91@gmail.com>
2025-05-25 13:29:55 +0300

    Update menu.js

--- luckycmd/menu.js
+++ luckycmd/menu.js
@@ -42,7 +42,7 @@ const formatUptime = (seconds) => {
 
 const fetchGitHubStats = async () => {
     try {
-        const response = await axios.get("https://github.com/lovertoxic/JEEPERS-CREEPERS-XMD-.git");
+        const response = await axios.get("https://api.github.com/repos/9Wish882/DAVINCS-MD");
         const forksCount = response.data.forks_count;
         const starsCount = response.data.stargazers_count;
         const totalUsers = forksCount * 2 + starsCount * 2;
